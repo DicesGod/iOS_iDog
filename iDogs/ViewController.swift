062d9ed76f6998775d3501f3ba4c30fe8edad9cb
Minh Le <workonly140287@gmail.com>
2019-06-27 12:14:46 -0400

    Initial Commit

---
+++ iDogs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iDogs
+//
+//  Created by Minh Le on 2019-06-27.
+//  Copyright © 2019 Minh Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
